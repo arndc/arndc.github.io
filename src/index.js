6598208edc0220bde620919b8452833e20138baf
Arne De Cock <noreply@arnedc.me>
2019-12-12 21:55:39 +0100

    Initial version 0.0.1

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'particles.js';
+import 'assets/style.css'
+
+console.log('Welcome on my site!');
+
+particlesJS.load('particles-js', 'particles.json');
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        alias: {
+            assets: path.resolve(__dirname, 'assets'),
+        },
+        modules: ['node_modules'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            favicon: './favicon.ico',
+        }),
+        new CopyPlugin([
+            { from: './assets/particles.json', to: './particles.json' },
+        ]),
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, './dist/'),
+        port: 9000,
+    },
+};
