7544f8afb2a0ac49fda5ae8e26a1195b3998e858
Arne De Cock <noreply@arnedc.me>
2020-01-31 23:37:34 +0100

    Add asset categories themes (with Bulma) and typography (with font Lato)
    Also contains vue.config.js theme configuration

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,17 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = {
     lintOnSave: false,
+    chainWebpack: config => {
+        config.module
+            .rule('scss')
+            .oneOf('vue')
+            .use('resolve-url-loader')
+            .loader('resolve-url-loader').options({ keepQuery: true })
+            .before('sass-loader');
+    },
     configureWebpack: {
         plugins: [
             new CleanWebpackPlugin({
@@ -10,5 +19,18 @@ module.exports = {
             }),
             new CopyPlugin(['README.md', 'CNAME']),
         ],
+        resolve: {
+            alias: {
+                '~theme': path.resolve(__dirname, 'src/assets/themes'),
+            },
+        },
+    },
+    css: {
+        sourceMap: true,
+        loaderOptions: {
+            sass: {
+                prependData: `@import "~theme/main";`,
+            },
+        },
     },
 };
