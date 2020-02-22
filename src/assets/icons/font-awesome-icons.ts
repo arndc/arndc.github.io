86f3b38d843ece2985e540ba7fbacc10c148f082
Arne De Cock <noreply@arnedc.me>
2020-02-22 16:44:12 +0100

    Add auto scroll with chevron on landing page

--- src/assets/icons/font-awesome-icons.ts
+++ src/assets/icons/font-awesome-icons.ts
@@ -1,8 +1,17 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faLaughBeam } from '@fortawesome/free-regular-svg-icons';
-import { faFeatherAlt, faHiking, faMoon, faPlusCircle, faRoute, faSun } from '@fortawesome/free-solid-svg-icons';
+import {
+    faChevronDown,
+    faFeatherAlt,
+    faHiking,
+    faMoon,
+    faPlusCircle,
+    faRoute,
+    faSun
+} from '@fortawesome/free-solid-svg-icons';
 
 library.add(
+    faChevronDown,
     faFeatherAlt,
     faHiking,
     faLaughBeam,
--- src/components/DcAboutMe.vue
+++ src/components/DcAboutMe.vue
@@ -1,53 +1,53 @@
 <template>
-  <main class="hero">
-    <div class="hero-body is-paddingless">
-      <div class="tile is-vertical has-text-centered">
+  <main class="is-paddingless">
 
-        <section class="dc-section" data-aos="fade-up">
-          <h2 class="title dc-title">Bio</h2>
-          <div class="dc-bio is-flex">
-            <figure class="image" data-aos="zoom-in-up" data-aos-anchor=".dc-bio">
-              <img class="is-rounded" src="@/assets/images/me.jpg" alt="Me (Arne De Cock)">
-            </figure>
-            <div class="has-text-left dc-texts">
-              <p class="box" data-aos="fade-up" data-aos-delay="400" data-aos-anchor=".dc-texts">
-                Hi, my name is Arne De Cock and welcome to my site
-                <font-awesome-icon class="dc-icon" :icon="['far', 'laugh-beam']" />
-              </p>
-              <p class="box" data-aos="fade-up" data-aos-delay="600" data-aos-anchor=".dc-texts">
-                I'm a developer by day
-                <font-awesome-icon class="dc-icon" :icon="['fa', 'sun']" />
-                <span class="dc-code has-text-weight-bold"> && </span>
-                also by night
-                <font-awesome-icon class="dc-icon" :icon="['fa', 'moon']" />
-              </p>
-              <p class="box" data-aos="fade-up" data-aos-delay="800" data-aos-anchor=".dc-texts">
-                Next to that I really love watching TV series, movies and playing games
-                <span class="dc-hashtag">#online #offline</span>
-              </p>
-              <p class="box" data-aos="fade-up" data-aos-delay="1000" data-aos-anchor=".dc-texts">
-                Oh yeah, one more thing. Be aware of my puns, they are on point!
-                <span class="dc-hashtag">#punmaster</span>
-              </p>
-            </div>
+    <section class="hero" data-aos="fade-up">
+      <section class="hero-body dc-section">
+        <h2 class="title dc-title">Bio</h2>
+        <div class="dc-bio is-flex">
+          <figure class="image" data-aos="zoom-in-up" data-aos-anchor=".dc-bio">
+            <img class="is-rounded" src="@/assets/images/me.jpg" alt="Me (Arne De Cock)">
+          </figure>
+          <div class="has-text-left dc-texts">
+            <p class="box" data-aos="fade-up" data-aos-delay="400" data-aos-anchor=".dc-texts">
+              Hi, my name is Arne De Cock and welcome to my site
+              <font-awesome-icon class="dc-icon" :icon="['far', 'laugh-beam']" />
+            </p>
+            <p class="box" data-aos="fade-up" data-aos-delay="600" data-aos-anchor=".dc-texts">
+              I'm a developer by day
+              <font-awesome-icon class="dc-icon" :icon="['fa', 'sun']" />
+              <span class="dc-code has-text-weight-bold"> && </span>
+              also by night
+              <font-awesome-icon class="dc-icon" :icon="['fa', 'moon']" />
+            </p>
+            <p class="box" data-aos="fade-up" data-aos-delay="800" data-aos-anchor=".dc-texts">
+              Next to that I really love watching TV series, movies and playing games
+              <span class="dc-hashtag">#online #offline</span>
+            </p>
+            <p class="box" data-aos="fade-up" data-aos-delay="1000" data-aos-anchor=".dc-texts">
+              Oh yeah, one more thing. Be aware of my puns, they are on point!
+              <span class="dc-hashtag">#punmaster</span>
+            </p>
           </div>
-        </section>
+        </div>
+      </section>
+    </section>
 
-        <section class="dc-section" data-aos="fade-up" data-aos-duration="800">
-          <h2 class="title dc-title">Trademarks</h2>
-          <div class="dc-container is-fullwidth">
-            <div class="columns">
-              <dc-card
-                  class="column is-4" v-for="properties in cardPropertiesList"
-                  :key="properties.title"
-                  :card-properties="properties"
-              />
-            </div>
+    <div class="hero" data-aos="fade-up" data-aos-delay="0">
+      <section class="hero-body dc-section">
+        <h2 class="title dc-title">Trademarks</h2>
+        <div class="dc-container is-fullwidth">
+          <div class="columns">
+            <dc-card
+                class="column is-4" v-for="properties in cardPropertiesList"
+                :key="properties.title"
+                :card-properties="properties"
+            />
           </div>
-        </section>
-
-      </div>
+        </div>
+      </section>
     </div>
+
   </main>
 </template>
 
@@ -80,7 +80,6 @@
     background: url('../assets/images/backgrounds/foggy-mountains.png') no-repeat,
     linear-gradient(180deg, $background 0%, opacify($background-darker, 0.7399334733893557) 33%, $background 85%);
     background-size: contain;
-    padding-bottom: 3.2rem;
 
     @media only screen and (min-width: $widescreen) {
       background: url('../assets/images/backgrounds/foggy-mountains-widescreen.png') no-repeat center top,
@@ -90,7 +89,22 @@
     }
 
     .dc-section {
-      margin: 2rem 0;
+      margin: 0;
+      display: flex;
+      flex-direction: column;
+      padding-top: 0;
+
+      @media only screen and (min-width: $tablet) {
+        padding: 0;
+
+        &:first-child {
+          padding-top: 2rem;
+        }
+
+        &:last-child {
+          padding-bottom: 10rem;
+        }
+      }
 
       .dc-title {
         $dc-padding-vertical: 1.23rem;
@@ -99,16 +113,17 @@
         $dc-border-width: 0.05rem;
 
         display: inline-block;
+        margin: 1.25rem 0 2.5rem;
         padding: $dc-padding-vertical $dc-padding-horizontal;
-        color: $primary-light;
         font-size: $dc-font-size;
         font-family: $dc-font-family-light;
         letter-spacing: .75rem;
+        text-align: center;
         text-transform: uppercase;
-        margin: 2.5rem 0;
+        color: $primary-light;
 
         @media only screen and (min-width: $tablet) {
-          padding: ($dc-padding-vertical * 2) ($dc-padding-horizontal * 1.2);
+          padding: ($dc-padding-vertical * 1.5) ($dc-padding-horizontal * 1.2);
           font-size: $dc-font-size * 1.64;
           letter-spacing: 1rem;
           border-width: $dc-border-width * 2;
